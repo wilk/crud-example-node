ccdbeaf97887601eeb471a63b1706629f5124c50
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2015-03-02 09:29:32 +0100

    added person ctrl

---
+++ app/controllers/person.js
@@ -0,0 +1,109 @@
+var personModel = require('../models/person'),
+    _ = require('lodash'),
+    logger = require('../utils/logger');
+
+function PersonCtrl () {
+    var me = this;
+
+    me.list = function (req, res) {
+        personModel.find({}, function (err, persons) {
+            if (err) {
+                logger.error(err);
+                return res.status(500).send(err.toString());
+            }
+
+            if (_.isEmpty(persons)) return res.status(404).send('No persons found');
+
+            persons.forEach(function (person, index) {
+                persons[index] = person.toJSON();
+            });
+
+            res.json(persons);
+        });
+    };
+
+    me.create = function (req, res) {
+        personModel.findOne({name: req.body.name, surname: req.body.surname, age: req.body.age}, function (err, person) {
+            if (err) {
+                logger.error(err);
+                return res.status(500).send(err.toString());
+            }
+
+            if (!_.isEmpty(person)) return res.status(400).send('User with this data already exists');
+
+            person = new personModel({
+                name: req.body.name,
+                surname: req.body.surname,
+                age: req.body.age
+            });
+
+            person.save(function (err) {
+                if (err) {
+                    logger.error(err);
+                    return res.status(500).send(err.toString());
+                }
+
+                res.json(person.toJSON());
+            });
+        });
+    };
+
+    me.single = function (req, res) {
+        personModel.findOne({'_id': req.params.personId}, function (err, person) {
+            if (err) {
+                logger.error(err);
+                return res.status(500).send(err.toString());
+            }
+
+            if (_.isEmpty(person)) return res.status(404).send('Person not found');
+
+            res.json(person.toJSON());
+        });
+    };
+
+    me.update = function (req, res) {
+        personModel.findOne({'_id': req.params.personId}, function (err, person) {
+            if (err) {
+                logger.error(err);
+                return res.status(500).send(err.toString());
+            }
+
+            if (_.isEmpty(person)) return res.status(404).send('Person not found');
+
+            person.name = req.body.name || person.name;
+            person.surname = req.body.surname || person.surname;
+            person.age = req.body.age || person.age;
+
+            person.save(function (err) {
+                if (err) {
+                    logger.error(err);
+                    return res.status(500).send(err.toString());
+                }
+
+                res.json(person.toJSON());
+            });
+        });
+    };
+
+    me.remove = function (req, res) {
+        personModel.findOne({'_id': req.params.personId}, function (err, person) {
+            if (err) {
+                logger.error(err);
+                return res.status(500).send(err.toString());
+            }
+
+            if (_.isEmpty(person)) return res.status(404).send('Person not found');
+
+            person.remove(function (err) {
+                if (err) {
+                    logger.error(err);
+                    return res.status(500).send(err.toString());
+                }
+
+                res.end();
+            });
+        });
+    };
+}
+
+module.exports = new PersonCtrl();
